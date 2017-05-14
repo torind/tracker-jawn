bc11d183794f06b03ccfb7f311f93808735e470b
Torin <torindisalvo@gmail.com>
2017-05-14 14:18:40 -0400

    Initial Commit

---
+++ Tracker JawnTests/Tracker_JawnTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tracker_JawnTests.swift
+//  Tracker JawnTests
+//
+//  Created by Torin on 5/14/17.
+//  Copyright © 2017 Torin. All rights reserved.
+//
+
+import XCTest
+@testable import Tracker_Jawn
+
+class Tracker_JawnTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
